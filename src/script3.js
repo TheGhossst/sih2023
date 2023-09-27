09b9c76f6a21bc8eda77c4eeeede58014df0bca7
TheGhossst <nandukrishnapkm97@gmail.com>
2023-09-27 19:19:06 +0530

    add :finale

--- src/script3.js
+++ src/script3.js
@@ -1,13 +1,22 @@
-// Get all grid squares
 const gridSquares = document.querySelectorAll('.grid-square');
-
-// Add a click event listener to each grid square
 gridSquares.forEach((square) => {
     square.addEventListener('click', toggleImageSelection);
 });
-
-// Function to toggle the selected class when a grid square is clicked
 function toggleImageSelection(event) {
     const square = event.currentTarget;
     square.classList.toggle('selected');
 }
+const loginLink = document.getElementById('loginLink');
+loginLink.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const popupMessage = 'You have been logged in.';
+    alert(popupMessage);
+    const waitForPopupClose = setInterval(() => {
+        if (!document.hidden) {
+            clearInterval(waitForPopupClose);
+            window.location.href = 'index.html'; 
+        }
+    }, 1000);
+});
+
